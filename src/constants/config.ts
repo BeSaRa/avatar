c82cc8c2e70e925a9b945a751bd245162c021ddc
BeSaRa <ahmedbesara@gmailcom>
2024-10-01 17:23:58 +0300

    remove version and update packages

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,11 +1,10 @@
 export const Config = {
-  VERSION: 'v0.0.1',
+  VERSION: 'v0.0.0',
   BASE_ENVIRONMENT: '',
   ENVIRONMENTS_URLS: {},
   BASE_URL: '',
   API_VERSION: 'v1',
   EXTERNAL_PROTOCOLS: ['http', 'https'],
-  TOKEN_HEADER_KEY: 'Authorization',
 }
 
 export type ConfigType = typeof Config
