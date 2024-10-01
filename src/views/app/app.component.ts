9097a3276586053dc707e5392e7851f13159131d
BeSaRa <ahmedbesara@gmailcom>
2024-10-01 12:56:13 +0300

    add version script to the package json

--- src/views/app/app.component.ts
+++ src/views/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { VersionComponent } from '@/components/version/version.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, VersionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
