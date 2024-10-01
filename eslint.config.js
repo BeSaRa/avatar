a1187f63a6948e4e1516a2d6f72f4ffdd1cd8b73
BeSaRa <ahmedbesara@gmailcom>
2024-10-01 11:30:27 +0300

    create project structure and install needed packages

---
+++ eslint.config.js
@@ -0,0 +1,60 @@
+// @ts-check
+// noinspection NpmUsedModulesInstalled
+const eslint = require('@eslint/js')
+const tsEsLint = require('typescript-eslint')
+const angular = require('angular-eslint')
+const unusedImports = require('eslint-plugin-unused-imports')
+
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended')
+
+module.exports = tsEsLint.config(
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    extends: [
+      eslint.configs.recommended,
+      ...tsEsLint.configs.recommended,
+      ...tsEsLint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      eslintPluginPrettierRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      'max-len': ['error', 120],
+      'no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: {},
+  }
+)
