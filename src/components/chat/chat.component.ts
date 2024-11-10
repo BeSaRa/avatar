752f03d06bc8cce4aa487b72cd48e92c3c2ba410
BeSaRa <ahmedbesara@gmailcom>
2024-11-10 16:44:53 +0300

    create main structure for chat

--- src/components/chat/chat.component.ts
+++ src/components/chat/chat.component.ts
@@ -1,6 +1,7 @@
-import { Component, computed, inject, signal } from '@angular/core'
+import { Component, computed, ElementRef, inject, signal, viewChild } from '@angular/core'
 import { MatRipple } from '@angular/material/core'
 import { LocalService } from '@/services/local.service'
+import { DOCUMENT } from '@angular/common'
 
 @Component({
   selector: 'app-chat',
@@ -12,9 +13,22 @@ import { LocalService } from '@/services/local.service'
 export class ChatComponent {
   status = signal(false)
   statusWord = computed(() => (this.status() ? 'opened' : 'closed'))
+  document = inject(DOCUMENT)
   lang = inject(LocalService)
+  chatContainer = viewChild.required<ElementRef<HTMLDivElement>>('chatContainer')
+  fullscreenStatus = signal(false)
 
   toggleChat() {
     this.status.update(value => !value)
+  }
+
+  fullScreenToggle() {
+    if (!this.document.fullscreenElement) {
+      this.chatContainer()
+        .nativeElement.requestFullscreen()
+        .then(() => this.fullscreenStatus.set(true))
+    } else {
+      this.document.exitFullscreen().then(() => this.fullscreenStatus.set(false))
+    }
   }
 }
