14f5346f94f1f51757d77e25e3a50641ba88e1e8
Moaed Alloula <moaed.alloula@invi-aligners.com>
2024-12-17 17:34:23 +0300

    feat: choose the bot to interact with in avatar and chat popup

--- src/services/chat.service.ts
+++ src/services/chat.service.ts
@@ -1,6 +1,8 @@
+import { distinctUntilChanged, tap } from 'rxjs'
 import { Injectable, signal } from '@angular/core'
 import { Message } from '@/models/message'
 import { BaseChatService } from './base-chat.service'
+import { FormControl } from '@angular/forms'
 
 @Injectable({
   providedIn: 'root',
@@ -9,4 +11,15 @@ export class ChatService extends BaseChatService {
   messages = signal<Message[]>([])
   status = signal<boolean>(false)
   conversationId = signal<string>('')
+  botNameCtrl = new FormControl('website', { nonNullable: true })
+
+  onBotNameChange() {
+    return this.botNameCtrl.valueChanges.pipe(
+      distinctUntilChanged(),
+      tap(() => {
+        this.conversationId.set('')
+        this.messages.set([])
+      })
+    )
+  }
 }
