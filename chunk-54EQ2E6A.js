97fdab5dd2785222fc3122e1173763e6433e3ca1
Moaed Alloula <moaed.alloula@invi-aligners.com>
2025-01-18 23:27:49 +0300

    Auto-generated commit

---
+++ chunk-54EQ2E6A.js
@@ -0,0 +1,1 @@
+import{q as t}from"./chunk-2CJENMGW.js";function n(e){return class extends e{destroy$=new t;ngOnDestroy(){this.destroy$.next(),this.destroy$.complete(),this.destroy$.unsubscribe()}}}export{n as a};
