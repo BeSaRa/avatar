45634c4c923183d27f85498fc31748a8b0ed026e
Moaed Alloula <moaed.alloula@invi-aligners.com>
2025-02-03 23:29:44 +0300

    Auto-generated commit

---
+++ chunk-67N5DM6I.js
@@ -0,0 +1,1 @@
+import{c as t}from"./chunk-JN2FAFAE.js";var r=class{context;conversation_id;content;role;id;constructor(){this.id=t()}isUser(){return this.role==="user"}isAssistant(){return this.role==="assistant"}isError(){return this.role==="error"}};export{r as a};
