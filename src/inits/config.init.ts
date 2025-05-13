ad2c2dc295deef958c21114b4935d48305cb2f38
Moaed Alloula <moaed.alloula@invi-aligners.com>
2025-05-13 09:52:09 +0300

    fix: call token api after login

--- src/inits/config.init.ts
+++ src/inits/config.init.ts
@@ -1,10 +1,9 @@
 import { APP_INITIALIZER, Injector, Provider } from '@angular/core'
 import { ConfigService } from '@/services/config.service'
 import { UrlService } from '@/services/url.service'
-import { catchError, forkJoin, of, switchMap, tap } from 'rxjs'
+import { forkJoin, switchMap, tap } from 'rxjs'
 import { SpeechService } from '@/services/speech.service'
 import { LocalService } from '@/services/local.service'
-import { AppStore } from '@/stores/app.store'
 import { ApplicationUserService } from '@/views/auth/services/application-user.service'
 
 export default {
@@ -20,16 +19,6 @@ export default {
       forkJoin([configService.load()]).pipe(
         tap(() => urlService.setConfigService(configService)),
         tap(() => urlService.prepareUrls()),
-        switchMap(() =>
-          commonService.generateSpeechToken().pipe(
-            tap(response => injector.get(AppStore).updateSpeechToken(response)),
-            catchError(error => {
-              console.error('Error generating speech token:', error)
-              // Optionally log the error or set a fallback state
-              return of(null) // Return a fallback observable
-            })
-          )
-        ),
         switchMap(() => local.load()),
         tap(() => injector.get(ApplicationUserService).checkAutoLogoutOnRefresh())
       )
